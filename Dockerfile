c55c287f714abc8490d41b3125b233f5737fa092
shreyanhota <shreyanhota@gmail.com>
2024-09-02 03:06:08 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# Use the official Rasa image as a base
+FROM python:3.8-slim
+
+# Set working directory
+WORKDIR /app
+USER root
+# Copy your flask project files into the container
+COPY . /app
+
+USER root
+
+
+
+# Copy any additional custom requirements, if necessary (uncomment next line)
+RUN pip install --no-cache-dir flask requests
+
+USER 1001
+
+# Expose ports for flask  server
+EXPOSE 5006
+
+# Run app.py when the container launches
+
+# CMD ["sh", "-c", "python app_rasa.py"]
+
+CMD ["sh", "-c", "sleep 60 && python app_rasa.py"]
+
+
+
+
+
+
+
+
+
---
+++ app_rasa.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+# Rasa server URL
+RASA_URL = "http://rasa:5005/webhooks/rest/webhook"
+
+
+@app.route('/')
+def index():
+    return render_template('index_rasa.html')
+
+@app.route('/webhook', methods=['POST'])
+def send_message():
+    user_message = request.json.get('message')
+    
+    # Send the user's message to the Rasa server
+    response = requests.post(
+        RASA_URL,
+        json={"sender": "user", "message": user_message}
+    )
+    
+    # Check if the response contains data
+    if response.ok and response.json():
+        rasa_response = response.json()
+        bot_message = rasa_response[0].get('text', "Sorry, I didn't understand that.")
+    else:
+        bot_message = "Sorry, I didn't get that. Could you please rephrase?"
+
+    return jsonify({"message": bot_message})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5006, debug=True)
+
+
+
+
+
+# def webhook():
+    # user_message = request.json.get('message')
+    # response = get_response(user_message)
+    # return jsonify({'text': response})
+
+# if __name__ == '__main__':
+    # app.run(debug=True,port=5006)
+
+
+
+
+
+
+
